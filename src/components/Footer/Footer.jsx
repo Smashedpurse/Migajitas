12782db2452c5dc0b89ac5c4b2db15bcb5361241
Ivan Rubio <smashedpurse@gmail.com>
2023-01-03 21:38:37 -0600

    Insert the information of the company into the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <footer>Este es un pie de pagina :D</footer>
+    <footer>
+        <div className='p-10 bg-black text-gray-200'>
+            <div className='max-w-7xl mx-auto'>
+                <div className='grid grid-cols-1 md:cols-2 lg:grid-cols-4 gap-2'>
+                   <div className=' mb-5'>
+                        <p>Company</p>
+                   </div>
+                   <div className=' mb-5 '>
+
+                   </div>
+                   <div className=' mb-5'>
+
+                   </div>
+                   <div className=' mb-5'>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </footer>
   )
 }
 
