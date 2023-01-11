5f5369e52e0543b17106dc29f945f9df842b9569
Ivan Rubio <smashedpurse@gmail.com>
2023-01-11 13:53:20 -0600

    Develope the first version of the app}

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import mapa from "../../images/mapa.PNG"
-const Category = () => {
+import Map from "../GoogleMap/Map"
+import credentials from "../../credentials"
+
+const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapsKey}`
+
+const Category = (props) => {
   return (
     <div className="max-w-[1640px] m-auto px-4 py-6">
       <h1 className="text-orange-600 font-bold text-4xl text-center mb-4">
@@ -16,10 +20,16 @@ const Category = () => {
           <p>Telefono: 5612081391</p>
         </strong>
         {/* Google maps */}
-          <img src={mapa} alt="First Location"/>
+        <Map
+          googleMapURL= {mapURL}
+          containerElement= {<div style={{height: '400px'}}/>}
+          mapElement= {<div style={{height: '100%'}}/>}
+          loadingElement= {<p>Cargando</p>}
+        />
       </div>
     </div>
   );
 };
+
 
 export default Category;
---
+++ src/credentials.js
@@ -0,0 +1,3 @@
+export default {
+    mapsKey:'AIzaSyD35n9cEhisl_yiWFa39V9VLjjFGQ4clfs'
+}
