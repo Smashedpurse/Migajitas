fa56be1d2cedd6345f792d6139ba12aa9c45b6ab
Ivan Rubio <smashedpurse@gmail.com>
2023-01-12 11:13:20 -0600

    creation of the card carpet to provide a bette view of the location

---
+++ src/components/CardLocation/CardLocation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CardLocation = () => {
+  return (
+    <div className=''>
+        <img/>
+        <div>
+            <h2>Title</h2>
+            <p>Category</p>
+            <p>Description</p>
+        </div>
+    </div>
+  )
+}
+
+export default CardLocation
--- src/credentials.js
+++
@@ -1,3 +0,0 @@
-export default {
-    mapsKey:'AIzaSyD35n9cEhisl_yiWFa39V9VLjjFGQ4clfs'
-}
